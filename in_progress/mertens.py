5205ffdafd5a8f9dcbcbe097c41fb34fccdf5d52
Lucas A. Brown <115374136+lucasaugustus@users.noreply.github.com>
2025-02-20 20:38:00 -0800

    Update mertens.py

--- in_progress/mertens.py
+++ in_progress/mertens.py
@@ -2,39 +2,7 @@
 
 from labmath3 import *
 
-def mertens_unsegmented(x):
-    # This is the unsegmented version of the Deleglise-Rivat algorithm.
-    # On my system, under PyPy3, this computes mertens(10**14) == -875575 in 5 minutes exactly,
-    # but uses 38.7 GB of memory along the way.  Segmentation should cut that down by a factor of 100,000 or so.
-    if x < 8: return (0, 1, 0, -1, -1, -2, -1, -2)[x]
-    u = introot(x, 3)
-    mobs = [0] + list(mobiussieve(x//u + 2))
-    merts = mobs[:]
-    for i in range(1, len(merts)): merts[i] = merts[i-1] + merts[i]
-    S1 = 0
-    for m in range(1, u+1):
-        if mobs[m] == 0: continue
-        innersum = 0
-        for n in range(-(u//-m), isqrt(x//m)+1):    # -(u//-m) == ceil(u/m)
-            innersum += merts[x//(m*n)]
-        S1 += mobs[m] * innersum
-    S2 = 0
-    for k in range(1, isqrt(x)+1):
-        innersum = 0
-        for m in range(1, min(u, x//(k*k))+1):
-            # l is the number of integers n in the interval (sqrt(y), y] such that y // n == k.
-            # These are the n such that k <= x/(mn) < k+1, or equivalently, x / ((k+1)*m) < n <= x / (m*k).
-            lo1 = isqrt(x//m) + 1
-            hi1 = x // m
-            lo2 = x // (m * (k+1)) + 1
-            hi2 = x // (m * k)
-            lo = max(lo1, lo2)
-            hi = min(hi1, hi2)
-            l = hi - lo + 1
-            if l < 0: l = 0
-            innersum += mobs[m] * l
-        S2 += merts[k] * innersum
-    return (merts[u] - S1 - S2, merts[u], S1, S2)
+# https://projecteuclid.org/journals/experimental-mathematics/volume-5/issue-4/Computing-the-summation-of-the-M%c3%b6bius-function/em/1047565447.full
 
 def mobiusblock(a, b, primes):
     """
@@ -67,18 +35,16 @@ def mertensblock(a, b, Ma, primes):
     # At this point, M[k] == mobius(a+k).
     M[0] = Ma
     for n in range(1, b-a): M[n] += M[n-1]
-    # We now have M[k] == Mertens(k).
+    # We now have M[k] == Mertens(a+k).
     return M
 
-def mertens_segmented(x):
-    if x < 21: return (0, 1, 0, -1, -1, -2, -1, -2, -2, -2, -1, -2, -2, -3, -2, -1, -1, -2, -2, -3, -3)[x]
-    u = introot(int(x * log(log(x))**2 * 1.0), 3)                     # TODO: experiment with various values for the multiplier.
-    primes16 = list(primegen(isqrt(u)+1))
-    mobs_u = mobiusblock(0, u+1, primes16)
+def mertens(x):
+    if x < 22: return (0, 1, 0, -1, -1, -2, -1, -2, -2, -2, -1, -2, -2, -3, -2, -1, -1, -2, -2, -3, -3, -2)[x]
+    u = int((x * log(log(x))**2)**(1/3))
+    primes = list(primegen(isqrt(x//u) + 1))
+    mobs_u = mobiusblock(0, u+1, primes)
     merts_u = [0] * (u+1)
     for n in range(u+1): merts_u[n] = merts_u[n-1] + mobs_u[n]
-    
-    primes13 = list(primegen(isqrt(x//u) + 1))
     
     # Here beginneth the S1 phase.
     
@@ -86,7 +52,8 @@ def mertens_segmented(x):
     # We will sieve the Mertens function in blocks of size u.
     lo = isqrt(x//u)
     hi = lo + u
-    lastM = merts_u[lo]
+    M_lo = merts_u[lo]
+    merts_u = merts_u[u]
     
     # We need to sieve Mertens up to the maximum value of x/mn that gets passed into it.
     # This means that we need to find the minimum value of mn in the sum.
@@ -97,10 +64,11 @@ def mertens_segmented(x):
     # m must be strictly greater than this, and this bound does not vary with the sieving iteration.
     global_min_m = max(0, u**2//x)   # This bound becomes nontrivial when c > sqrt(x) / log(log(x))^2.
     
-    isqrtxm = {m:isqrt(x//m) for m in range(1, u+1)}
+    isqrtxm = [0]
+    isqrtxm.extend([isqrt(x//m) for m in range(1, u+1)])
     
     while lo <= max_xmn:
-        merts_lo = mertensblock(lo, hi+1, lastM, primes13)
+        merts_lo = mertensblock(lo, hi+1, M_lo, primes)
         
         # TODO: This implementation proceeds by, within each sieving block, iterating over m in an outer loop and then
         # iterating over n in an inner one.  Would it be better to iterate over n in the outer loop and m in the inner?
@@ -110,31 +78,12 @@ def mertens_segmented(x):
         
         The inequalities that characterize the valid (m,n) pairs are:
         
-        1.  u/m < n
-        2.  n <= sqrt(x/m)
-        3.  lo <= x/mn
-        4.  x/mn < hi
-        5.  1 <= m
-        6.  m <= u
+            1: u/m < n    2: n <= sqrt(x/m)    3: lo <= x/mn    4: x/mn < hi    5: 1 <= m    6: m <= u
         
-        What is the range of m such that there exists an n in u/m < n <= sqrt(x/m) such that lo <= x/mn < hi?
-        
-        For a given m, n is constrained by:
-        
-        1.  u/m < n
-        2.  n^2 <= x / m
-        3.  n <= x / (m*lo)
-        4.  x / (m*hi) < n
-        
-        Additionally, m is constrained by
-        
-        5.  1 <= m
-        6.  m <= u
-        
-        We therefore have three lower bounds (1,4) on n and three upper bounds (2,3).
+        For a given m, we therefore have two lower bounds (1,4) on n and two upper bounds (2,3).
         
         (1) & (2) imply      u^2 < x m      u^2 / x < m
-        (1) & (3) imply   u * lo < x        This does not constrain m, but its failure is a signal that the S1 phase is done.
+        (1) & (3) imply   u * lo < x        This does not constrain anything; its failure is a signal that the S1 phase is done.
         (4) & (2) imply        x < m hi^2   x / hi^2 < m
         (4) & (3) imply       lo < hi       This is always true.
         
@@ -143,7 +92,7 @@ def mertens_segmented(x):
         
         For a given m, the range of admissible n-values is bounded by (2) and (4):
         
-        x / (m*hi) < n   and   n^2 <= x / m
+            x / (m*hi) < n   and   n^2 <= x / m
         
         If we are to have our outer loop be m and the inner loop be n, then we will be calculating many square roots.
         However, the range of admissible m-values for the whole S1 phase is 1 <= m <= u.
@@ -157,13 +106,15 @@ def mertens_segmented(x):
         for m in range(min_m, u+1):
             min_n = max(1, x//(m*hi) + 1, (u//m) + 1)   # n >= this
             max_n = min(isqrtxm[m], x//(m*lo))          # n <= this
+            subtotal = 0
             for n in range(min_n, max_n+1):
                 xmn = x // (m*n)
                 if lo <= xmn < hi:
-                    S1 += mobs_u[m] * merts_lo[xmn-lo]
+                    subtotal += merts_lo[xmn-lo]
+            S1 += mobs_u[m] * subtotal
         
         lo, hi = hi, hi + u
-        lastM = merts_lo[u]
+        M_lo = merts_lo[u]
     
     # Here endeth the S1 phase.
     # Here beginneth the S2 phase.
@@ -189,7 +140,23 @@ def mertens_segmented(x):
     
     # Here endeth the S2 phase.
     
-    return merts_u[u] - S1 - S2
+    return merts_u - S1 - S2
 
-from sys import argv
-print(mertens_segmented(int(argv[1])))
+def test_mertens():
+    M = 0
+    for (n,m) in enumerate(mobiussieve(2**14), start=1):
+        M += m
+        MM = mertens(n)
+        assert M == MM, (n, M, MM)
+    A084237 = [1, -1, 1, 2, -23, -48, 212, 1037, 1928, -222, -33722, -87856]
+    for n in range(len(A084237)):
+        M = mertens(10**n)
+        assert M == A084237[n], (n, M, A084237[n])
+    A084236 = [1, 0, -1, -2, -1, -4, -1, -2, -1, -4, -4, 7, -19, 22, -32, 26, 14, -20, 24, -125, 257, -362,\
+               228, -10, 211, -1042, 329, 330, -1703, 6222, -10374, 9569, 1814, -10339, -3421, 8435, 38176]
+    for n in range(len(A084236)):
+        M = mertens(2**n)
+        assert M == A084236[n], (n, M, A084236[n])
+
+#test_mertens()
+from sys import argv; print(mertens(int(argv[1])))
